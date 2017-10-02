16775e41799997b24fcc1bcb4f47ddf66e126393
Arnaud Dhaene <arnaud.dhaene@epfl.ch>
2017-10-02 10:42:16 +0200

    neuron
    added neuron header and cpp files

---
+++ cmake-build-debug/CMakeFiles/brunel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/brunel.dir/neuron.cpp.o"
+  "brunel.pdb"
+  "brunel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/brunel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
