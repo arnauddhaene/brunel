5bb9d374942b8083eb2c939e903b9bd7cb973336
Arnaud Dhaene <arnaud.dhaene@epfl.ch>
2017-11-02 17:36:43 +0100

    cmake-build-debug removed

--- cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arnauddhaene/Documents/GitHub/cppcourse-brunel/googletest
-# Build directory: /Users/arnauddhaene/Documents/GitHub/cppcourse-brunel/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
